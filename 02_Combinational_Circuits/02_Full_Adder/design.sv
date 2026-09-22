7e009b39511aed7b17f6330a2a1d2f82e72b7fff
RudraDakve <dakverudra2@gmail.com>
2026-09-22 07:15:35 +0530

    Add full adder module in design.sv

---
+++ 02_Combinational_Circuits/02_Full_Adder/design.sv
@@ -0,0 +1,5 @@
+// Full Adder Module.
+module full_adder (input wire a, input wire b, input wire cin, output wire sum, output wire carry);
+  assign sum = a ^ b ^ cin;
+    assign carry = (a & b) | (b & cin) | (cin & a);
+endmodule
